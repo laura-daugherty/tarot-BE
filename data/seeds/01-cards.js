deac2f8174e4b8b15427db927b7bf4cf10ae0cad
Laura Daugherty <lauradaugherty6@gmail.com>
2020-01-29 17:00:12 -0800

    updating seeds through major arcana

--- data/seeds/01-cards.js
+++ data/seeds/01-cards.js
@@ -1,8 +1,39 @@
 
 exports.seed = function(knex) {
   return knex('cards').insert([
-    {id: 1, name: 'testvalue'},
-    {id: 2, name: 'test'},
-    {id: 3, name: 'tarot'}
+    {id: 0, name: 'The Fool', suit: '', order: '', description: '', cardimage:'', cardimage:'https://upload.wikimedia.org/wikipedia/en/9/90/RWS_Tarot_00_Fool.jpg'},
+    {id: 1, name: 'The Magician', suit: '', order: '', description: 'skill, diplomacy, address, sickness, pain, loss, disaster, self-confidence, will, the querent himself', cardimage:'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg'},
+    {id: 2, name: 'The High Priestess', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/8/88/RWS_Tarot_02_High_Priestess.jpg'},
+    {id: 3, name: 'The Empress', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/d/d2/RWS_Tarot_03_Empress.jpg'},
+    {id: 4, name: 'The Emperor', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/c/c3/RWS_Tarot_04_Emperor.jpg'},
+    {id: 5, name: 'The Hierophant', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/8/8d/RWS_Tarot_05_Hierophant.jpg'},
+    {id: 6, name: 'The Lovers', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/d/db/RWS_Tarot_06_Lovers.jpg'},
+    {id: 7, name: 'The Chariot', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/9/9b/RWS_Tarot_07_Chariot.jpg'},
+    {id: 8, name: 'Strength', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/f/f5/RWS_Tarot_08_Strength.jpg'},
+    {id: 9, name: 'The Hermit', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/4/4d/RWS_Tarot_09_Hermit.jpg'},
+    {id: 10, name: 'Wheel of Fortune', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/3/3c/RWS_Tarot_10_Wheel_of_Fortune.jpg'},
+    {id: 11, name: 'Justice', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/e/e0/RWS_Tarot_11_Justice.jpg'},
+    {id: 12, name: 'The Hanged Man', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/2/2b/RWS_Tarot_12_Hanged_Man.jpg'},
+    {id: 13, name: 'Death', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/d/d7/RWS_Tarot_13_Death.jpg'},
+    {id: 14, name: 'Temperance', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/f/f8/RWS_Tarot_14_Temperance.jpg'},
+    {id: 15, name: 'The Devil', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/5/55/RWS_Tarot_15_Devil.jpg'},
+    {id: 16, name: 'The Tower', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/5/53/RWS_Tarot_16_Tower.jpg'},
+    {id: 17, name: 'The Star', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/d/db/RWS_Tarot_17_Star.jpg'},
+    {id: 18, name: 'The Moon', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/7/7f/RWS_Tarot_18_Moon.jpg'},
+    {id: 19, name: 'The Sun', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/1/17/RWS_Tarot_19_Sun.jpg'},
+    {id: 20, name: 'Judgement', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/d/dd/RWS_Tarot_20_Judgement.jpg'},
+    {id: 21, name: 'The World', suit: '', order: '', description: '', cardimage:'https://upload.wikimedia.org/wikipedia/en/f/ff/RWS_Tarot_21_World.jpg'},
+    {id: 22, name: '', suit: '', order: '', description: '', cardimage:''},
+    {id: 23, name: '', suit: '', order: '', description: '', cardimage:''},
+    {id: 24, name: '', suit: '', order: '', description: '', cardimage:''},
+    {id: 25, name: '', suit: '', order: '', description: '', cardimage:''},
+    {id: 26, name: '', suit: '', order: '', description: '', cardimage:''},
+    {id: 27, name: '', suit: '', order: '', description: '', cardimage:''},
+    {id: 28, name: '', suit: '', order: '', description: '', cardimage:''},
+    {id: 29, name: '', suit: '', order: '', description: '', cardimage:''},
+    {id: 30, name: '', suit: '', order: '', description: '', cardimage:''},
+    {id: 31, name: '', suit: '', order: '', description: '', cardimage:''},
+    {id: 32, name: '', suit: '', order: '', description: '', cardimage:''},
+
   ]);
 };
